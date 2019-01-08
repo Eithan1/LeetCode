3c0339741c9eae82e06b104186270edc89af2653
Eithan1 <44333487+Eithan1@users.noreply.github.com>
2019-01-08 21:24:26 +0800

    Create main.cpp

---
+++ 40. Combination Sum II/main.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+        vector<vector<int>> res;
+        if(candidates.size() == 0)
+            return res;
+        vector<int> cur;
+        sort(candidates.begin(), candidates.end());
+        BackTracking(res, cur, candidates, target, 0);
+        return res;
+    }
+
+    void BackTracking(vector<vector<int>>& res, vector<int> cur, vector<int> candidates, int target, int index) {
+        if(target == 0) {
+            res.push_back(cur);
+            return;
+        }
+        else if(target < 0)
+            return;
+        else if(target < candidates[index])  //如果需要的数比当前最小的还要小，则没有必要尝试
+            return;
+
+        for(int i = index; i < candidates.size(); ++i) {
+            target -= candidates[i]; //尝试将candidates[i]放入
+            cur.push_back(candidates[i]);
+            BackTracking(res, cur, candidates, target, i + 1); //数字不可重复所以是i+1，而不是i
+            cur.pop_back(); //将candidates[i] pop出来，继续尝试下一个
+            target += candidates[i];
+            while(i < candidates.size() - 1 && candidates[i] == candidates[i + 1]) //防止重复
+                ++i;
+        }
+    }
+};

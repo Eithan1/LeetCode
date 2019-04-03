9782298e125545ea9feff45b05a63f6155a175ca
Eithan1 <44333487+Eithan1@users.noreply.github.com>
2019-04-03 10:52:56 +0800

    Update main.cpp

--- 90. Subsets II/main.cpp
+++ 90. Subsets II/main.cpp
@@ -1,18 +1,20 @@
+
 class Solution {
 public:
     void dfs(vector<vector<int>>& res, vector<int>& one_res, vector<int>& nums, int level) {
-        res.push_back(one_res);
-
-        if(one_res.size() == res.size()) {
+        if(one_res.size() == nums.size()) { //退出条件
+            res.push_back(one_res);         //记录answer
             return;
         }
-
-        for(int i = level; i < nums.size(); ++i) {
-            if(i > level && nums[i] == nums[i - 1])
-                continue;
-            one_res.push_back(nums[i]);
-            dfs(res, one_res, nums, i + 1);
-            one_res.pop_back();
+        else {
+            res.push_back(one_res);         //此题较为特殊，当个数小的时候仍要记录answer
+            for(int i = level; i < nums.size(); ++i) {
+                if(i > level && nums[i] == nums[i - 1])  //判断有重复元素则跳过
+                    continue;
+                one_res.push_back(nums[i]);     //元素入栈
+                dfs(res, one_res, nums, i + 1); //深搜
+                one_res.pop_back();             //元素出栈（此处还原所有条件）
+            }
         }
     }
 
